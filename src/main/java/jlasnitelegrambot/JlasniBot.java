dc2d42807f8f2979be020b1ba00f88a34ee9c1d8
Victor Suarez <suarez.garcia.victor@gmail.com>
2017-11-08 22:34:43 +0000

    commit inicial

---
+++ src/main/java/jlasnitelegrambot/JlasniBot.java
@@ -0,0 +1,5 @@
+package jlasnitelegrambot;
+
+public class JlasniBot {
+
+}
---
+++ src/test/java/jlasnitelegrambot/JlasniBotTest.java
@@ -0,0 +1,24 @@
+package jlasnitelegrambot;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class JlasniBotTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
